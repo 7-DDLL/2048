52ea4945b877776be11f325e0bc02ac99e72482c
7-DDLL <2799534934@qq.com>
2022-05-30 21:51:29 +0800

    Create GameView.vue

---
+++ src/views/GameView.vue
@@ -0,0 +1,36 @@
+<template>
+    <!-- <router-view/> -->
+  <div class="game">
+    <h1>test</h1>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.topNav{
+  // margin: 1rem;
+  // width: 20rem;
+  height: 56dp;
+  // display: flex;
+  // justify-content: space-between;
+  // align-items: center;
+  // padding: 0 0.2rem;
+  text-align: center;
+  background: rgb(229, 122, 161);
+  
+  .topCenter{
+    text-align: center;
+    // width: 7rem;
+    display: flex;
+    justify-content: space-around;
+    font-size: large;
+    color: white;
+    .active{
+      font-weight: 900;
+    }
+    .navBtn{
+      margin: 5px;
+    }
+  }
+
+}
+</style>
