992452dca01770d7d8db52dc0880ee670c81b22a
7-DDLL <2799534934@qq.com>
2022-05-30 21:51:35 +0800

    Create ShopView.vue

---
+++ src/views/ShopView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="other">
+    <top-nav></top-nav>
+    
+    <shop-list></shop-list>
+
+    <shop-bottom class="bottom"></shop-bottom>
+  </div>
+</template>
+
+<script>
+import topNav from '@/components/topNav.vue'
+import shopList from '@/components/shopList.vue'
+import ShopBottom from '@/components/shopBottom.vue'
+
+export default {
+  name: 'ShopView',
+  components: {
+    topNav,
+    shopList,
+    ShopBottom
+}
+}
+</script>
+
+<style scoped>
+.other{
+    text-align: center;
+    background: #faf8ef;
+    height: 1000px;
+}
+.swiper{
+    width: 200px;
+    border: 1px solid rgb(238, 235, 235);
+    
+    padding: 1px;
+    border-radius: 15px;
+    background: white;
+}
+.left{
+    float: left;
+}
+.all{
+    height: 160px;
+}
+.bottom{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
